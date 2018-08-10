da19ee01647813ec1b725f430d04fad05b9bcb00
cris415 <cristianreynoso@icloud.com>
2018-08-10 14:46:32 -0400

    Add post build script to heroku and serve static files from server if production logic

--- index.js
+++ index.js
@@ -36,5 +36,16 @@ app.get('/api/random-quote', async (req, res) => {
   res.send(quote[0]);
 });
 
+if (process.env.NODE_ENV === 'production') {
+  //  Serve prodution assets like a particular file
+  app.use(express.static('client/build'));
+
+  // Serve index.html file for paths not in the server
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
