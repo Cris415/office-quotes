0c438deb7d0e6290e6f5a45caa419c70128e93c0
cris415 <cristianreynoso@icloud.com>
2018-08-14 17:05:24 -0400

    Remove images folder from client and add a public setting to express server

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+
+  app.use(express.static('public'));
 }
 
 const PORT = process.env.PORT || 5000;
